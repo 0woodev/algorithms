bbef33a64e15c5679405de82751cd97b2eedef18
0woodev <0woodev@gmail.com>
2023-05-05 16:38:38 +0900

    BOOK: 문자열 다루기 기본 solve (level 1) algorithms: regex

---
+++ src/main/java/org/owoodev/book/jbhyunikim/handle_string_regex/Solution.java
@@ -0,0 +1,7 @@
+package org.owoodev.book.jbhyunikim.handle_string_regex;
+
+public class Solution {
+    public boolean solution(String s) {
+        return s.matches("[0-9]{4}|[0-9]{6}");
+    }
+}
