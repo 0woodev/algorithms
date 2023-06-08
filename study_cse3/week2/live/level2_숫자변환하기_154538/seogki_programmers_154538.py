88d8c7ce51d2ec33d570c326efd05f2bf96640ca
ikgoes <seogki.k@gmail.com>
2023-06-08 23:39:11 +0900

    edited week2 live seogki code

--- study_cse3/week2/live/level2_숫자변환하기_154538/seogki_programmers_154538.py
+++ study_cse3/week2/live/level2_숫자변환하기_154538/seogki_programmers_154538.py
@@ -9,7 +9,34 @@ def solution(x, y, n):
         y, cnt = q.popleft()
         visited[y] = True
         if x == y: return cnt
-        if y%3==0 and y//3 >= x and not visited[y//3]: q.append([y//3, cnt+1]) 
-        if y%2==0 and y//2 >= x and not visited[y//2]: q.append([y//2, cnt+1]) 
-        if y-n >= x and not visited[y-n]: q.append([y-n, cnt+1]) 
+        if y%3==0 and y//3 >= x and not visited[y//3]: 
+            q.append([y//3, cnt+1]) 
+        if y%2==0 and y//2 >= x and not visited[y//2]: 
+            q.append([y//2, cnt+1]) 
+        if y-n >= x and not visited[y-n]: 
+            q.append([y-n, cnt+1]) 
     return -1
+
+"""
+from collections import deque
+
+def solution(x, y, n): 
+    q = deque() 
+    q.append([x,0])
+    visited = [False] * 1000001
+    visited[x] = True
+    while(q): 
+        x, cnt = q.popleft()
+        visited[x] = True
+        if x == y: return cnt
+        if x*3 <= y and not visited[x*3]: 
+            visited[x*3] = True
+            q.append([x*3, cnt+1]) 
+        if x*2 <= y and not visited[x*2]: 
+            visited[x*2] = True
+            q.append([x*2, cnt+1]) 
+        if x+n <= y and not visited[x+n]:
+            visited[x+n] = True
+            q.append([x+n, cnt+1]) 
+    return -1
+"""
